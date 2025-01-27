9cb2ff8e2c2a9f3bf6ec398464f7f428e8caf9fc
ArzuAmrah <arzu.amrahova@bk.ru>
2025-01-27 17:28:55 +0400

    Dəyişikliklərin izahı

--- src/main/java/az/atl/bookshop/configuration/CorsConfiguration.java
+++ src/main/java/az/atl/bookshop/configuration/CorsConfiguration.java
@@ -1,4 +1,26 @@
 package az.atl.bookshop.configuration;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
 public class CorsConfiguration {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer(){
+
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry corsRegistry) {
+                corsRegistry
+                        .addMapping("/**")
+                        .allowedOrigins("GET","POST","PUT","DELETE")
+                        .allowedOrigins("*");
+            }
+        };
+
+    }
 }
--- src/main/java/az/atl/bookshop/configuration/SecurityConfig.java
+++ src/main/java/az/atl/bookshop/configuration/SecurityConfig.java
@@ -1,13 +1,46 @@
 package az.atl.bookshop.configuration;
 
+
+import az.atl.bookshop.filters.JwtRequestFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
-    @Bean
-    public BCryptPasswordEncoder bCryptPasswordEncoder() {
-        return new BCryptPasswordEncoder();
+
+    private final JwtRequestFilter authFilter;
+
+    @Autowired
+    public SecurityConfig(JwtRequestFilter authFilter) {
+        this.authFilter = authFilter;
+    }
+
+    @Bean(name = "customSecurityFilterChain")
+    public SecurityFilterChain customSecurityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .headers(header -> header.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/authenticate/**", "/sign-up/**").permitAll()
+                        .requestMatchers("/api/**").authenticated()
+                )
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .addFilterBefore(authFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
     }
 }
+
+
